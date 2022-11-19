a62a8ecb3a99c126dba01e368120bbe6008a0b00
KirttiphoomEarth <ket15032545@gmail.com>
2022-11-19 23:46:03 +0700

    pin config

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -18,7 +18,6 @@
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
-#include "string.h"
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
@@ -55,26 +54,6 @@ uint32_t CountTim3_PrintCheck;		//Print Debug
 /* USER CODE END PM */
 
 /* Private variables ---------------------------------------------------------*/
-#if defined ( __ICCARM__ ) /*!< IAR Compiler */
-#pragma location=0x2007c000
-ETH_DMADescTypeDef  DMARxDscrTab[ETH_RX_DESC_CNT]; /* Ethernet Rx DMA Descriptors */
-#pragma location=0x2007c0a0
-ETH_DMADescTypeDef  DMATxDscrTab[ETH_TX_DESC_CNT]; /* Ethernet Tx DMA Descriptors */
-
-#elif defined ( __CC_ARM )  /* MDK ARM Compiler */
-
-__attribute__((at(0x2007c000))) ETH_DMADescTypeDef  DMARxDscrTab[ETH_RX_DESC_CNT]; /* Ethernet Rx DMA Descriptors */
-__attribute__((at(0x2007c0a0))) ETH_DMADescTypeDef  DMATxDscrTab[ETH_TX_DESC_CNT]; /* Ethernet Tx DMA Descriptors */
-
-#elif defined ( __GNUC__ ) /* GNU Compiler */
-ETH_DMADescTypeDef DMARxDscrTab[ETH_RX_DESC_CNT] __attribute__((section(".RxDecripSection"))); /* Ethernet Rx DMA Descriptors */
-ETH_DMADescTypeDef DMATxDscrTab[ETH_TX_DESC_CNT] __attribute__((section(".TxDecripSection")));   /* Ethernet Tx DMA Descriptors */
-
-#endif
-
-ETH_TxPacketConfig TxConfig;
-
-ETH_HandleTypeDef heth;
 
 TIM_HandleTypeDef htim3;
 TIM_HandleTypeDef htim4;
@@ -90,7 +69,6 @@ PCD_HandleTypeDef hpcd_USB_OTG_FS;
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
-static void MX_ETH_Init(void);
 static void MX_USART3_UART_Init(void);
 static void MX_USB_OTG_FS_PCD_Init(void);
 static void MX_TIM4_Init(void);
@@ -144,7 +122,6 @@ int main(void)
 
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
-  MX_ETH_Init();
   MX_USART3_UART_Init();
   MX_USB_OTG_FS_PCD_Init();
   MX_TIM4_Init();
@@ -258,55 +235,6 @@ void SystemClock_Config(void)
   {
     Error_Handler();
   }
-}
-
-/**
-  * @brief ETH Initialization Function
-  * @param None
-  * @retval None
-  */
-static void MX_ETH_Init(void)
-{
-
-  /* USER CODE BEGIN ETH_Init 0 */
-
-  /* USER CODE END ETH_Init 0 */
-
-   static uint8_t MACAddr[6];
-
-  /* USER CODE BEGIN ETH_Init 1 */
-
-  /* USER CODE END ETH_Init 1 */
-  heth.Instance = ETH;
-  MACAddr[0] = 0x00;
-  MACAddr[1] = 0x80;
-  MACAddr[2] = 0xE1;
-  MACAddr[3] = 0x00;
-  MACAddr[4] = 0x00;
-  MACAddr[5] = 0x00;
-  heth.Init.MACAddr = &MACAddr[0];
-  heth.Init.MediaInterface = HAL_ETH_RMII_MODE;
-  heth.Init.TxDesc = DMATxDscrTab;
-  heth.Init.RxDesc = DMARxDscrTab;
-  heth.Init.RxBuffLen = 1524;
-
-  /* USER CODE BEGIN MACADDRESS */
-
-  /* USER CODE END MACADDRESS */
-
-  if (HAL_ETH_Init(&heth) != HAL_OK)
-  {
-    Error_Handler();
-  }
-
-  memset(&TxConfig, 0 , sizeof(ETH_TxPacketConfig));
-  TxConfig.Attributes = ETH_TX_PACKETS_FEATURES_CSUM | ETH_TX_PACKETS_FEATURES_CRCPAD;
-  TxConfig.ChecksumCtrl = ETH_CHECKSUM_IPHDR_PAYLOAD_INSERT_PHDR_CALC;
-  TxConfig.CRCPadCtrl = ETH_CRC_PAD_INSERT;
-  /* USER CODE BEGIN ETH_Init 2 */
-
-  /* USER CODE END ETH_Init 2 */
-
 }
 
 /**
@@ -479,6 +407,7 @@ static void MX_GPIO_Init(void)
   GPIO_InitTypeDef GPIO_InitStruct = {0};
 
   /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOE_CLK_ENABLE();
   __HAL_RCC_GPIOC_CLK_ENABLE();
   __HAL_RCC_GPIOF_CLK_ENABLE();
   __HAL_RCC_GPIOH_CLK_ENABLE();
@@ -488,10 +417,35 @@ static void MX_GPIO_Init(void)
   __HAL_RCC_GPIOD_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOB, LD1_Pin|LD3_Pin|LD2_Pin, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOF, GPIO_PIN_4|GPIO_PIN_11|GPIO_PIN_14|GPIO_PIN_15, GPIO_PIN_RESET);
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(USB_PowerSwitchOn_GPIO_Port, USB_PowerSwitchOn_Pin, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_0|GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_11
+                          |GPIO_PIN_12|GPIO_PIN_14|GPIO_PIN_15|GPIO_PIN_7, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOE, GPIO_PIN_7|GPIO_PIN_8|GPIO_PIN_9|GPIO_PIN_11
+                          |GPIO_PIN_0, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOD, GPIO_PIN_14|GPIO_PIN_15, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOG, GPIO_PIN_4|GPIO_PIN_5|USB_PowerSwitchOn_Pin|GPIO_PIN_8
+                          |GPIO_PIN_11|GPIO_PIN_13, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_6, GPIO_PIN_RESET);
+
+  /*Configure GPIO pins : PE2 PE3 PE4 PE5
+                           PE6 PE10 PE12 PE13
+                           PE14 PE15 PE1 */
+  GPIO_InitStruct.Pin = GPIO_PIN_2|GPIO_PIN_3|GPIO_PIN_4|GPIO_PIN_5
+                          |GPIO_PIN_6|GPIO_PIN_10|GPIO_PIN_12|GPIO_PIN_13
+                          |GPIO_PIN_14|GPIO_PIN_15|GPIO_PIN_1;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(GPIOE, &GPIO_InitStruct);
 
   /*Configure GPIO pin : USER_Btn_Pin */
   GPIO_InitStruct.Pin = USER_Btn_Pin;
@@ -499,88 +453,166 @@ static void MX_GPIO_Init(void)
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   HAL_GPIO_Init(USER_Btn_GPIO_Port, &GPIO_InitStruct);
 
-  /*Configure GPIO pins : PF7 PF8 PF9 */
-  GPIO_InitStruct.Pin = GPIO_PIN_7|GPIO_PIN_8|GPIO_PIN_9;
+  /*Configure GPIO pins : PF0 PF1 PF2 PF3
+                           PF5 PF6 PF7 PF8
+                           PF9 PF10 PF12 PF13 */
+  GPIO_InitStruct.Pin = GPIO_PIN_0|GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3
+                          |GPIO_PIN_5|GPIO_PIN_6|GPIO_PIN_7|GPIO_PIN_8
+                          |GPIO_PIN_9|GPIO_PIN_10|GPIO_PIN_12|GPIO_PIN_13;
   GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   HAL_GPIO_Init(GPIOF, &GPIO_InitStruct);
 
-  /*Configure GPIO pins : LD1_Pin LD3_Pin LD2_Pin */
-  GPIO_InitStruct.Pin = LD1_Pin|LD3_Pin|LD2_Pin;
+  /*Configure GPIO pins : PF4 PF11 PF14 PF15 */
+  GPIO_InitStruct.Pin = GPIO_PIN_4|GPIO_PIN_11|GPIO_PIN_14|GPIO_PIN_15;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOF, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : PC0 PC2 PC3 PC7
+                           PC8 PC9 PC10 PC11
+                           PC12 */
+  GPIO_InitStruct.Pin = GPIO_PIN_0|GPIO_PIN_2|GPIO_PIN_3|GPIO_PIN_7
+                          |GPIO_PIN_8|GPIO_PIN_9|GPIO_PIN_10|GPIO_PIN_11
+                          |GPIO_PIN_12;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : PA0 PA3 PA4 PA5
+                           PA15 */
+  GPIO_InitStruct.Pin = GPIO_PIN_0|GPIO_PIN_3|GPIO_PIN_4|GPIO_PIN_5
+                          |GPIO_PIN_15;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : PB0 PB1 PB2 PB11
+                           PB12 PB14 PB15 PB7 */
+  GPIO_InitStruct.Pin = GPIO_PIN_0|GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_11
+                          |GPIO_PIN_12|GPIO_PIN_14|GPIO_PIN_15|GPIO_PIN_7;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
-  /*Configure GPIO pins : PG1 USB_OverCurrent_Pin */
-  GPIO_InitStruct.Pin = GPIO_PIN_1|USB_OverCurrent_Pin;
+  /*Configure GPIO pins : PG0 PG1 PG2 PG3
+                           USB_OverCurrent_Pin PG9 PG10 PG12
+                           PG14 PG15 */
+  GPIO_InitStruct.Pin = GPIO_PIN_0|GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3
+                          |USB_OverCurrent_Pin|GPIO_PIN_9|GPIO_PIN_10|GPIO_PIN_12
+                          |GPIO_PIN_14|GPIO_PIN_15;
   GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   HAL_GPIO_Init(GPIOG, &GPIO_InitStruct);
 
-  /*Configure GPIO pin : USB_PowerSwitchOn_Pin */
-  GPIO_InitStruct.Pin = USB_PowerSwitchOn_Pin;
+  /*Configure GPIO pins : PE7 PE8 PE9 PE11
+                           PE0 */
+  GPIO_InitStruct.Pin = GPIO_PIN_7|GPIO_PIN_8|GPIO_PIN_9|GPIO_PIN_11
+                          |GPIO_PIN_0;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-  HAL_GPIO_Init(USB_PowerSwitchOn_GPIO_Port, &GPIO_InitStruct);
+  HAL_GPIO_Init(GPIOE, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : PB10 PB4 PB5 PB6
+                           PB8 PB9 */
+  GPIO_InitStruct.Pin = GPIO_PIN_10|GPIO_PIN_4|GPIO_PIN_5|GPIO_PIN_6
+                          |GPIO_PIN_8|GPIO_PIN_9;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : PD10 PD11 PD12 PD13
+                           PD0 PD1 PD2 PD3
+                           PD4 PD5 PD6 PD7 */
+  GPIO_InitStruct.Pin = GPIO_PIN_10|GPIO_PIN_11|GPIO_PIN_12|GPIO_PIN_13
+                          |GPIO_PIN_0|GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3
+                          |GPIO_PIN_4|GPIO_PIN_5|GPIO_PIN_6|GPIO_PIN_7;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(GPIOD, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : PD14 PD15 */
+  GPIO_InitStruct.Pin = GPIO_PIN_14|GPIO_PIN_15;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOD, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : PG4 PG5 USB_PowerSwitchOn_Pin PG8
+                           PG11 PG13 */
+  GPIO_InitStruct.Pin = GPIO_PIN_4|GPIO_PIN_5|USB_PowerSwitchOn_Pin|GPIO_PIN_8
+                          |GPIO_PIN_11|GPIO_PIN_13;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOG, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : PC6 */
+  GPIO_InitStruct.Pin = GPIO_PIN_6;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
 
 }
 
 /* USER CODE BEGIN 4 */
-static bool checkWhereCanGo(bool round_player, uint8_t player_id, uint8_t row, uint8_t col, uint8_t type){
-	if(player_id != round_player){
-		return false; //wrong player playing
-	}
-	else{//true player playing
-		if(player_id == 0){ // above player
-			if(type == 1){ // beer
-				if(row != 0){
-					if(isEmpthy(row-1,col)){
-						tableCanGo[row-1][col] = true;
-					}
-					if(col>0 && col <=7 && isEnemy(round_player, round_player, row-1, col-1)){
-						tableCanGo[row-1][col-1] = true;
-					}
-					if(col>=0 && col <7 && isEnemy(round_player, round_player, row-1, col+1)){
-						tableCanGo[row-1][col+1] = true;
-					}
-				}
-				else { //change beer to beer ngai
-					type = 7;
-					chess[player_id][row][col] = 7;
-				}
-			}
-			else if (type == 2){ // boat
-				for(uint8_t i = 0; i < 8; i++){
-					if(isEmpthy(row, i) || isEnemy(round_player, round_player, row, i)){
-						tableCanGo[row][i] = true;
-					}
-					if(isEmpthy(i,col) || isEnemy(round_player, round_player, i, col)){
-						tableCanGo[i][col] = true;
-					}
-				}
-			}
-			else if (type == 3){ // horse
-				if()
-			}
-		}
-	}
-}
-
-static uint8_t isEnemy(uint8_t round_player, uint8_t player_id, uint8_t row, uint8_t col){
-	if (round_player != player_id && chess[player_id][row][col] > 0){
-		return true;
-	}
-	return false;
-}
-
-static bool isEmpthy(uint8_t row, uint8_t col){
-	if (chess[0][row][col] == 0 && chess[1][row][col] == 0){
-		return true;
-	}
-	return false;
-}
+//static bool checkWhereCanGo(bool round_player, uint8_t player_id, uint8_t row, uint8_t col, uint8_t type){
+//	if(player_id != round_player){
+//		return false; //wrong player playing
+//	}
+//	else{//true player playing
+//		if(player_id == 0){ // above player
+//			if(type == 1){ // beer
+//				if(row != 0){
+//					if(isEmpthy(row-1,col)){
+//						tableCanGo[row-1][col] = true;
+//					}
+//					if(col>0 && col <=7 && isEnemy(round_player, round_player, row-1, col-1)){
+//						tableCanGo[row-1][col-1] = true;
+//					}
+//					if(col>=0 && col <7 && isEnemy(round_player, round_player, row-1, col+1)){
+//						tableCanGo[row-1][col+1] = true;
+//					}
+//				}
+//				else { //change beer to beer ngai
+//					type = 7;
+//					chess[player_id][row][col] = 7;
+//				}
+//			}
+//			else if (type == 2){ // boat
+//				for(uint8_t i = 0; i < 8; i++){
+//					if(isEmpthy(row, i) || isEnemy(round_player, round_player, row, i)){
+//						tableCanGo[row][i] = true;
+//					}
+//					if(isEmpthy(i,col) || isEnemy(round_player, round_player, i, col)){
+//						tableCanGo[i][col] = true;
+//					}
+//				}
+//			}
+//			else if (type == 3){ // horse
+//				if()
+//			}
+//		}
+//	}
+//}
+//
+//static uint8_t isEnemy(uint8_t round_player, uint8_t player_id, uint8_t row, uint8_t col){
+//	if (round_player != player_id && chess[player_id][row][col] > 0){
+//		return true;
+//	}
+//	return false;
+//}
+//
+//static bool isEmpthy(uint8_t row, uint8_t col){
+//	if (chess[0][row][col] == 0 && chess[1][row][col] == 0){
+//		return true;
+//	}
+//	return false;
+//}
 
 extern void displayNumber(uint32_t c)
 {
--- Core/Src/stm32f7xx_it.c
+++ Core/Src/stm32f7xx_it.c
@@ -20,7 +20,6 @@
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
 #include "stm32f7xx_it.h"
-
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
 #include <stdbool.h>
@@ -30,10 +29,13 @@
 /* USER CODE BEGIN TD */
 extern uint32_t CountTim4;
 extern uint32_t CountTim3_PrintCheck;
-extern bool NewState[8][8];
+//extern bool NewState[8][8];
 
-bool OldState[8][8];
-bool State[8][8];
+extern bool readState[8][8];
+uint8_t currentState[8][8][6][2];
+
+//bool OldState[8][8];
+//bool State[8][8];
 /* USER CODE END TD */
 
 /* Private define ------------------------------------------------------------*/
@@ -149,16 +151,6 @@ void UsageFault_Handler(void)
 /**
   * @brief This function handles System service call via SWI instruction.
   */
-void DebugMon_Handler(void)
-{
-  /* USER CODE BEGIN DebugMonitor_IRQn 0 */
-
-  /* USER CODE END DebugMonitor_IRQn 0 */
-  /* USER CODE BEGIN DebugMonitor_IRQn 1 */
-
-  /* USER CODE END DebugMonitor_IRQn 1 */
-}
-
 void SVC_Handler(void)
 {
   /* USER CODE BEGIN SVCall_IRQn 0 */
@@ -172,6 +164,16 @@ void SVC_Handler(void)
 /**
   * @brief This function handles Debug monitor.
   */
+void DebugMon_Handler(void)
+{
+  /* USER CODE BEGIN DebugMonitor_IRQn 0 */
+
+  /* USER CODE END DebugMonitor_IRQn 0 */
+  /* USER CODE BEGIN DebugMonitor_IRQn 1 */
+
+  /* USER CODE END DebugMonitor_IRQn 1 */
+}
+
 /**
   * @brief This function handles Pendable request for system service.
   */
@@ -214,8 +216,8 @@ void TIM3_IRQHandler(void)
   /* USER CODE BEGIN TIM3_IRQn 0 */
 	CountTim4++;
 	//displayNumber(CountTim4);
-	displayOld_State(OldState[7][0], OldState[6][0], OldState[5][0], OldState[4][0]);
-	displayNew_State(NewState[7][0], NewState[6][0], NewState[5][0], NewState[4][0]);
+//	displayOld_State(OldState[7][0], OldState[6][0], OldState[5][0], OldState[4][0]);
+//	displayNew_State(NewState[7][0], NewState[6][0], NewState[5][0], NewState[4][0]);
   /* USER CODE END TIM3_IRQn 0 */
   HAL_TIM_IRQHandler(&htim3);
   /* USER CODE BEGIN TIM3_IRQn 1 */
@@ -234,10 +236,95 @@ void TIM4_IRQHandler(void)
 	//	 a3 = HAL_GPIO_ReadPin(GPIOF, GPIO_PIN_7);
 	//	 a2 = HAL_GPIO_ReadPin(GPIOF, GPIO_PIN_9);
 	//	 a1 = HAL_GPIO_ReadPin(GPIOG, GPIO_PIN_1);
-	NewState[7][0] = HAL_GPIO_ReadPin(GPIOF, GPIO_PIN_8);
-	NewState[6][0] = HAL_GPIO_ReadPin(GPIOF, GPIO_PIN_7);
-	NewState[5][0] = HAL_GPIO_ReadPin(GPIOF, GPIO_PIN_9);
-	NewState[4][0] = HAL_GPIO_ReadPin(GPIOG, GPIO_PIN_1);
+//	NewState[7][0] = HAL_GPIO_ReadPin(GPIOF, GPIO_PIN_8);
+//	NewState[6][0] = HAL_GPIO_ReadPin(GPIOF, GPIO_PIN_7);
+//	NewState[5][0] = HAL_GPIO_ReadPin(GPIOF, GPIO_PIN_9);
+//	NewState[4][0] = HAL_GPIO_ReadPin(GPIOG, GPIO_PIN_1);
+
+//	readState[7][0] = HAL_GPIO_ReadPin(GPIOF, GPIO_PIN_8);// [row][col]
+//	readState[6][0] = HAL_GPIO_ReadPin(GPIOF, GPIO_PIN_7);
+//	readState[5][0] = HAL_GPIO_ReadPin(GPIOF, GPIO_PIN_9);
+//	readState[4][0] = HAL_GPIO_ReadPin(GPIOG, GPIO_PIN_1);
+//	extern bool extern bool readState[8][8];[8][8];
+	//==================== Colum [0] ======================//
+	readState[7][0][1] = HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_10);
+	readState[6][0] = HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_12);
+	readState[5][0] = HAL_GPIO_ReadPin(GPIOF, GPIO_PIN_6);
+	readState[4][0] = HAL_GPIO_ReadPin(GPIOF, GPIO_PIN_7);
+	readState[3][0] = HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_15);
+	readState[2][0] = HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_2);
+	readState[1][0] = HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_3);
+	readState[0][0] = HAL_GPIO_ReadPin(GPIOD, GPIO_PIN_4);
+
+	//==================== Colum [1] ======================//
+	readState[7][1] = HAL_GPIO_ReadPin(GPIOD, GPIO_PIN_5);
+	readState[6][1] = HAL_GPIO_ReadPin(GPIOD, GPIO_PIN_6);
+	readState[5][1] = HAL_GPIO_ReadPin(GPIOD, GPIO_PIN_7);
+	readState[4][1] = HAL_GPIO_ReadPin(GPIOE, GPIO_PIN_3);
+	readState[3][1] = HAL_GPIO_ReadPin(GPIOF, GPIO_PIN_1);
+	readState[2][1] = HAL_GPIO_ReadPin(GPIOF, GPIO_PIN_0);
+	readState[1][1] = HAL_GPIO_ReadPin(GPIOD, GPIO_PIN_1);
+	readState[0][1] = HAL_GPIO_ReadPin(GPIOD, GPIO_PIN_0);
+
+	//==================== Colum [2] ======================//
+	readState[7][2] = HAL_GPIO_ReadPin(GPIOG, GPIO_PIN_0);
+	readState[6][2] = HAL_GPIO_ReadPin(GPIOE, GPIO_PIN_1);
+	readState[5][2] = HAL_GPIO_ReadPin(GPIOG, GPIO_PIN_9);
+	readState[4][2] = HAL_GPIO_ReadPin(GPIOG, GPIO_PIN_12);
+	readState[3][2] = HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_11);
+	readState[2][2] = HAL_GPIO_ReadPin(GPIOD, GPIO_PIN_2);
+	readState[1][2] = HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_0);
+	readState[0][2] = HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_4);
+
+	//==================== Colum [3] ======================//
+	readState[7][3] = HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_0);
+	readState[6][3] = HAL_GPIO_ReadPin(GPIOD, GPIO_PIN_3);
+	readState[5][3] = HAL_GPIO_ReadPin(GPIOG, GPIO_PIN_2);
+	readState[4][3] = HAL_GPIO_ReadPin(GPIOG, GPIO_PIN_3);
+	readState[3][3] = HAL_GPIO_ReadPin(GPIOE, GPIO_PIN_2);
+	readState[2][3] = HAL_GPIO_ReadPin(GPIOE, GPIO_PIN_4);
+	readState[1][3] = HAL_GPIO_ReadPin(GPIOE, GPIO_PIN_5);
+	readState[0][3] = HAL_GPIO_ReadPin(GPIOF, GPIO_PIN_2);
+
+	//==================== Colum [4] ======================//
+	readState[7][4] = HAL_GPIO_ReadPin(GPIOF, GPIO_PIN_8);
+	readState[6][4] = HAL_GPIO_ReadPin(GPIOF, GPIO_PIN_9);
+	readState[5][4] = HAL_GPIO_ReadPin(GPIOG, GPIO_PIN_1);
+	readState[4][4] = HAL_GPIO_ReadPin(GPIOE, GPIO_PIN_6);
+	readState[3][4] = HAL_GPIO_ReadPin(GPIOG, GPIO_PIN_15);
+	readState[2][4] = HAL_GPIO_ReadPin(GPIOG, GPIO_PIN_10);
+	readState[1][4] = HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_3);
+	readState[0][4] = HAL_GPIO_ReadPin(GPIOF, GPIO_PIN_3);
+
+	//==================== Colum [5] ======================//
+	readState[7][5] = HAL_GPIO_ReadPin(GPIOF, GPIO_PIN_5);
+	readState[6][5] = HAL_GPIO_ReadPin(GPIOF, GPIO_PIN_10);
+	readState[5][5] = HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_8);
+	readState[4][5] = HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_9);
+	readState[3][5] = HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_8);
+	readState[2][5] = HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_9);
+	readState[1][5] = HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_5);
+	readState[0][5] = HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_6);
+
+	//==================== Colum [6] ======================//
+	readState[7][6] = HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_7);
+	readState[6][6] = HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_10);
+	readState[5][6] = HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_4);
+	readState[4][6] = HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_5);
+	readState[3][6] = HAL_GPIO_ReadPin(GPIOD, GPIO_PIN_13);
+	readState[2][6] = HAL_GPIO_ReadPin(GPIOD, GPIO_PIN_12);
+	readState[1][6] = HAL_GPIO_ReadPin(GPIOD, GPIO_PIN_11);
+	readState[0][6] = HAL_GPIO_ReadPin(GPIOE, GPIO_PIN_10);
+
+	//==================== Colum [7] ======================//
+	readState[7][7] = HAL_GPIO_ReadPin(GPIOE, GPIO_PIN_12);
+	readState[6][7] = HAL_GPIO_ReadPin(GPIOE, GPIO_PIN_14);
+	readState[5][7] = HAL_GPIO_ReadPin(GPIOE, GPIO_PIN_15);
+	readState[4][7] = HAL_GPIO_ReadPin(GPIOE, GPIO_PIN_13);
+	readState[3][7] = HAL_GPIO_ReadPin(GPIOF, GPIO_PIN_13);
+	readState[2][7] = HAL_GPIO_ReadPin(GPIOF, GPIO_PIN_12);
+	readState[1][7] = HAL_GPIO_ReadPin(GPIOG, GPIO_PIN_14);
+	readState[0][7] = HAL_GPIO_ReadPin(GPIOD, GPIO_PIN_10);
   /* USER CODE END TIM4_IRQn 0 */
   HAL_TIM_IRQHandler(&htim4);
   /* USER CODE BEGIN TIM4_IRQn 1 */
